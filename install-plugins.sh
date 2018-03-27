8296c51f3554fafa54285471d255384385362a9f
Ibrahim Wickama <ibrahimwickama@gmail.com>
2018-03-27 22:51:27 +0300

    fixed store version errors

--- install-plugins.sh
+++ install-plugins.sh
@@ -2,6 +2,6 @@
 npm install --save bootstrap@4.0.0-beta.2
 npm install --save font-awesome
 npm install --save @angular/cli@latest
-npm install --save @ngrx/store
+npm install --save @ngrx/store@4.1.0
 npm install --save ngx-pagination
 npm install --save angular-tree-component
